2a9759f11f4815458f2f001f41b48729ff240e32
KashifIV <kashif.h77@gmail.com>
2022-03-14 13:30:40 -0400

    initial commit

---
+++ congestion_visualization/src/Map/Map.js
@@ -0,0 +1,80 @@
+import React, { useRef, useEffect } from "react"; 
+import mapboxgl from "mapbox-gl";
+
+mapboxgl.accessToken = "pk.eyJ1Ijoia2FzaGlmaXYiLCJhIjoiY2t3YXdjaDhoOGJlcjJubXRvOG5lamJrMCJ9.o2nL-I7eqpqsWcBFwyIcgA"; 
+
+export const Map = () => {
+  const mapContainer = useRef(); 
+  console.log(process.env.MAPBOX_TOKEN);
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-79.3883, 43.6548],
+      zoom: 14,
+      pitch: 60,
+    }); 
+    map.on("load", () => {
+      map.addLayer(
+        {
+        'id': 'add-3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 10,
+        'paint': {
+        'fill-extrusion-color': '#aaa',
+         
+        // Use an 'interpolate' expression to
+        // add a smooth transition effect to
+        // the buildings as the user zooms in.
+        'fill-extrusion-height': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        10,
+        0,
+        10.05,
+        ['get', 'height']
+        ],
+        'fill-extrusion-base': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        10,
+        0,
+        10.05,
+        ['get', 'min_height']
+        ],
+        'fill-extrusion-opacity': 1.0
+        }
+        },
+        );
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        tileSize: 512,
+        maxZoom: 16,
+      });
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 })
+      map.addLayer({
+        id: "sky",
+        type: "sky",
+        paint: {
+          "sky-type": "atmosphere",
+          "sky-atmosphere-sun": [0.0, 90.0],
+          "sky-atmosphere-sun-intensity": 15,
+        },
+      });
+    });
+  });
+
+  return (
+    <div
+      id="map"
+      ref={mapContainer}
+      style={{ width: "100%", height: "100vh" }}
+    />
+  )
+};
